20d5de63c96804a0b83a3096084e1d958bf4d96e
modi0021 <modi0021@umn.edu>
2018-04-15 14:53:34 -0500

    build working version of app

---
+++ README.md
@@ -0,0 +1,50 @@
+# Flask app for Titanic: Machine Learning from Disaster
+
+This is flask app to predict if a particular passenger survived or not. Along with the label, it shows the 
+Survival probability too.
+
+## Packages Used:
+```
++ Json
++ Pandas
++ Numpy
++ Flask
++ WTForms
++ Scikit-Learn
+```
+
+## Steps:
+### Data preperation and model building
+```
++ Reading data
++ Splitting the data into training and testing data
++ Imputing missing values from training dataset.
++ Using values imputed from training data to fill missing values in testing dataset to avoid data leakage.
++ Encoding Categorical predictors using sklearn LabelEncoder
++ Fitting RandomForestClassifier with Training data
++ Serializing the model to disk
+```
+### Prediction on flask app
+```
++ Navigate to prediction page - http://localhost:5000/predict.
++ User fill the form in the browser with required predictors value and then submit the form.
++ Values provided by users are put into model to get predictions.
++ The result from the model is showed to user on browser.
+```
+
+## To run the app
++ Open terminal in main directory '/Titanic Flask App'.
++ Then run app.py from terminal
+
+```
+python app.py
+# This will start flask app server. 
+# Now can open the site by opening http://localhost:5000/ page.
+# NOTE: Right now, app runs in debug mode
+```
+
+## Future work
++ Improve the model
++ Provide drop down for user input
++ Take user input as string for categorical data, instead of current method i.e. numeric values.
++ Improve the design of site.
--- app.py
+++ app.py
@@ -1,73 +1,121 @@
-from sklearn.externals import joblib
+# importing the packages
+import json
 import pandas as pd
 import numpy as np
-from flask import Flask, request, jsonify
-import json
+from flask import Flask, request, jsonify, render_template, flash#, redirect, url_for, session, logging
+from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
+from sklearn.externals import joblib
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 
+# value of __name__ should be  '__main__'
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-	return 'Hello, World!'
+def index():
+	# Index page
+	return render_template('index.html')
 
+@app.route('/about')
+def about():
+	# about me page
+	return render_template('about.html')
+
+class PredictorsForm(Form):
+	"""
+	This is a form class to retrieve the input from user through form
+
+	Inherits: request.form class
+	"""
+	p_class = StringField(u'P Class', validators=[validators.input_required()])
+	sex = StringField(u'Sex', validators=[validators.input_required()])
+	age = StringField(u'Age', validators=[validators.input_required()])
+	sibsp = StringField(u'Siblings and Spouse Count', validators=[validators.input_required()])
+	parch = StringField(u'Parch', validators=[validators.input_required()])
+	fare = StringField(u'Fare', validators=[validators.input_required()])
+	embarked = StringField(u'Embarked', validators=[validators.input_required()])
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+	form = PredictorsForm(request.form)
+	# Checking if user submitted the form and the values are valid
+	if request.method == 'POST' and form.validate():
+		# Now save all values passed by user into variables
+		p_class = form.p_class.data
+		sex = form.sex.data
+		age = form.age.data
+		sibsp = form.sibsp.data
+		parch = form.parch.data
+		fare = form.fare.data
+		embarked = form.embarked.data
+		# Creating input for model for predictions
+		predict_request = [int(p_class), int(sex), float(age), int(sibsp), int(parch), float(fare), int(embarked)]
+		predict_request = np.array(predict_request).reshape(1, -1)
+		# Class predictions from the model
+		prediction = model.predict(predict_request)
+		prediction = str(prediction[0])
+		# Survival Probability from the model
+		predict_prob = model.predict_proba(predict_request)
+		predict_prob = str(predict_prob[0][1])
+		# Passing the predictions to new view(template)
+		return render_template('predictions.html', prediction=prediction, predict_prob=predict_prob)
+	return render_template('predict.html', form=form)
 
 @app.route('/train', methods=['GET'])
 def train():
-    # using random forest as an example
-    # can do the training separately and just update the pickles
-    
+	# reading data
 	df = pd.read_csv("./data/titanic.csv")
+
+	#defining predictors and label columns to be used
 	predictors = ['Pclass','Sex', 'Age', 'SibSp', 'Parch', 'Fare', 'Embarked']
 	label = 'Survived'
-	cabin_fillna = 'NA'
-	df.Cabin = df.Cabin.fillna(cabin_fillna)
 
+	#Splitting data into training and testing
 	df_train, df_test, y_train, y_test = train_test_split(df[predictors], df[label], test_size=0.20, random_state=42)
 
+	# Data cleaning and filling missing values
 	age_fillna = df_train.Age.mean()
 	embarked_fillna = df_train.Embarked.value_counts().index[0]
+	cabin_fillna = 'NA'
 
-
+	# filling missing values in training data
 	df_train.Age = df_train.Age.fillna(df.Age.mean())
 	df_train.Embarked = df_train.Embarked.fillna(embarked_fillna)
+	df_train.Cabin = df_train.Cabin.fillna(cabin_fillna)
 
+	# filling missing values imputed from training set to avoid data leakage
 	df_test.Age = df_test.Age.fillna(df.Age.mean())
 	df_test.Embarked = df_test.Embarked.fillna(embarked_fillna)
+	df_test.Cabin = df_test.Cabin.fillna(cabin_fillna)
 
+	# Label encoding of object type predictors
 	le = dict()
 	for column in df_train.columns:
 	    if df_train[column].dtype == np.object:
 	        le[column] = LabelEncoder()
 	        df_train[column] = le[column].fit_transform(df_train[column])
-	        
+
+	# Applying same encoding from training data to testing data
 	for column in df_test.columns:
 	    if df_test[column].dtype == np.object:
 	        df_test[column] = le[column].transform(df_test[column])
 
+	# Initializing the model
 	model = RandomForestClassifier(n_estimators=25, random_state=42)
-	model.fit(X=df_train, y=y_train)
-	# y_pred = model.predict(X=df_test)
-	# print(confusion_matrix(y_test, y_pred))
-	# print(f1_score(y_test, y_pred))
-	from sklearn.externals import joblib
-	joblib.dump(model, './model/model.pkl') 
 
+	# Fitting the model with training data
+	model.fit(X=df_train, y=y_train)
+
+	# Saving the trained model on disk
+	joblib.dump(model, './model/model.pkl')
+
+	# Return success message for user display on browser
 	return 'Success'
 
-
-@app.route('/predict', methods=['POST'])
-def predict():
-	# data = request.get_json(force=True)
-	data = json.loads(request.get_json(s))
-	predict_request = [data['Pclass'], data['Sex'], data['Age'], data['SibSp'], data['Parch'], data['Fare'], data['Embarked']]
-	predict_request = np.array(predict_request).reshape(1, -1)
-	prediction = model.predict(predict_request)
-	output = prediction[0]
-	return jsonify({'prediction':str(output), 'results': 'Success'})
-
 if __name__ == '__main__':
+	# Load the pre-trained model from the disk
 	model = joblib.load('./model/model.pkl')
-	app.run(port=8080)
+	# Running the app in debug mode allows to change the code and
+	# see the changes without the need to restart the server
+	app.run(debug=True)
